40282b6ff1f740a6b10475556f936568972a4e94
lzy <lzy@qq.com>
2020-03-15 14:50:40 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    Hello World
   </div>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  @import "./common/stylus/variable.styl"
+
+  #app
+    color $color-theme
+    background-color $color-background
 </style>
